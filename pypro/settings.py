f2dfd2ba0de4151583e62d8c25e91093d0767e40
jrcidade@gmail.com <Print("github")>
2022-02-10 20:26:35 -0400

    Config. Correção env-sample

--- pypro/settings.py
+++ pypro/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os.path
 from functools import partial
 from pathlib import Path
-
 import dj_database_url
 from decouple import config, Csv
 
