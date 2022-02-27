8a1dc969c48b57a2de114dfd71ec7c1c50543fc8
jrcidade@gmail.com <Print("github")>
2022-02-27 14:50:47 -0400

    Incorparando primeiro vídeo do vimeo de forma manual close #32

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -12,8 +12,10 @@ def resp(client):
 def test_status_code(resp):
     assert resp.status_code == 200
 
+
 def test_titulo_video(resp):
     assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
 
 def test_conteudo_video(resp):
     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/681146135')
