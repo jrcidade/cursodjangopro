b6db1f39ceba0efa2a6ca563d8f11f90bef74eca
jrcidade@gmail.com <Print("github")>
2022-02-28 21:29:12 -0400

    Utilizar Filtro no Título de video
    close #40

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 681146135},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação do Windows', 'vimeo_id': 681146168},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 681146135},
-        'instalacao-windows': {'titulo': 'Instalação do Windows', 'vimeo_id': 681146168},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
